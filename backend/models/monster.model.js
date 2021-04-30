e4a58d84fdeae44a6164c008c1b6a3e3b170a461
Matthew Cook <matthewcook13987@gmail.com>
2021-04-30 16:15:00 -0500

    adds basic data fields to monster schema

--- backend/models/monster.model.js
+++ backend/models/monster.model.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const monsterSchema = new Schema({
-
+  name: { type: String, required: true},
+  weaknesses: {type: Array, required: true },
+  locations: {type: Array, required: true },
 })
 
 const Monster = mongoose.model('Monster', monsterSchema);
