b2ef4778e8b3e2c97a2accce9da99fad41cd7642
Matthew Cook <matthewcook13987@gmail.com>
2021-04-30 16:15:36 -0500

    creates route to retrieve info from the db

---
+++ backend/routes/monsters.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+let Monster = require('../models/monster.model');
+
+router.route('/').get((req,res)=>{
+  Monster.find()
+    .then(monsters => res.json(monsters))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req,res)=> {
+  const name = req.body.name;
+  const newMonster = new Monster({name});
+
+  newMonster.save()
+    .then(() => res.json('Monster added!'))
+    .catch(err => res.status(400).json('Error: ' + err))
+});
+
+module.exports = router;
